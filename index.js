6053edd53d9b3cb9ea40cc609afd929fe6d8cadf
James Hassett <jhassett189@gmail.com>
2022-06-17 15:33:15 -0400

    commit tree bcade1308451addc4258aedf8e443f386f8d12bc

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    let names = drivers.filter(driver => driver.toLowerCase() == name.toLowerCase())
+    return names
+}
+function fuzzyMatch(drivers, first) {
+    const letters = drivers.filter(driver => driver.substring(0, 2) == first)
+    return letters
+}
+function matchName(drivers, name) {
+    const names = drivers.filter(driver => driver.name == name)
+    return names
+}
